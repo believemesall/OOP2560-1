bc18f51387d1a8754b5ab66524a6a25ec6b4fa47
Koson Trachu <koson.trachu@gmail.com>
2017-08-23 10:50:30 +0700

    add Thailand

--- hello_world/hello_world/Program.cs
+++ hello_world/hello_world/Program.cs
@@ -11,6 +11,8 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Thailand!");
+
         }
     }
 }
